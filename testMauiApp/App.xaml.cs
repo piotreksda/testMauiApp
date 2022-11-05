e517f8101b5a087f8b621a69d2f6295f94a14a64
piotreksda <piotreksda@gmail.com>
2022-11-05 17:36:23 +0100

    Test Console.WriteLine pass

--- testMauiApp/App.xaml.cs
+++ testMauiApp/App.xaml.cs
@@ -5,6 +5,7 @@ public partial class App : Application
 	public App()
 	{
 		InitializeComponent();
+        Console.WriteLine("Started the app");
         Application.Current.RequestedThemeChanged += (s, a) =>
         {
             Console.WriteLine("Theme changed");
